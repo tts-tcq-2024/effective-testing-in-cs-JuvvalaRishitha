f3ba09733367c8ecf43e7f880e04ccddd0f40d21
JuvvalaRishitha <juvvalarishitha@gmail.com>
2024-08-22 12:11:10 +0530

    Update tshirt.cs

--- tshirts/tshirt.cs
+++ tshirts/tshirt.cs
@@ -1,31 +1,41 @@
 using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    class Tshirt {
-        static string Size(int cms) {
-            if(cms < 38) {
+namespace TshirtSpace
+{
+    class Tshirt
+    {
+        // Method to determine T-shirt size based on measurements in cms
+        static string Size(int cms)
+        {
+            if (cms < 38)
+            {
                 return "S"; // Small
-            } else if(cms > 38 && cms < 42) {
+            }
+            else if (cms > 38 && cms < 42)
+            {
                 return "M"; // Medium
-            } else {
+            }
+            else
+            {
                 return "L"; // Large
             }
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             // Existing test cases
-            //Debug.Assert(Size(37) == "S");
-            //Debug.Assert(Size(40) == "M");
-            //Debug.Assert(Size(43) == "L");
+            Debug.Assert(Size(37) == "S"); // This should pass
+            Debug.Assert(Size(40) == "M"); // This should pass
+            Debug.Assert(Size(43) == "L"); // This should pass
 
             // New test cases designed to fail
-            Debug.Assert(Size(38) == "S"); // This should return "M", expecting failure
-            Debug.Assert(Size(42) == "M"); // This should return "L", expecting failure
-            Debug.Assert(Size(100) == "M"); // This should return "L", expecting failure
-            Debug.Assert(Size(0) == "M"); // This should return "S", expecting failure
-            Debug.Assert(Size(-10) == "S"); // This should return "S", but let's assume we're checking against invalid here
-            
+            Debug.Assert(Size(38) == "S"); // This will fail (expected "M")
+            Debug.Assert(Size(42) == "M"); // This will fail (expected "L")
+            Debug.Assert(Size(100) == "M"); // This will fail (expected "L")
+            Debug.Assert(Size(0) == "M"); // This will fail (expected "S")
+            Debug.Assert(Size(-10) == "S"); // This will pass (expected "S")
+
             Console.WriteLine("All is well (maybe!)");
         }
     }
